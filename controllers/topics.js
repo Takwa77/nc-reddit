e7cc4f5e451ccfdb0116e132c0ed4fdbc411fe1d
Takwa77 <takwa-t@hotmail.com>
2022-07-04 12:47:49 +0100

    add changes to controllers

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,7 +1,6 @@
 const { selectTopics } = require("../models/topics");
 
 exports.getTopics = (req, res) => {
-  console.log("im in controllers");
   //   selectTopics().then((topics) => {
   res.send(200).send(topics);
   //   });
