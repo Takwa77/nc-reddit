13fcd9d36b02dbb676119223b314f38967f46a96
Takwa77 <takwa-t@hotmail.com>
2022-07-05 16:41:13 +0100

    add sql changes

--- view-test-dbs.sql
+++ view-test-dbs.sql
@@ -3,6 +3,13 @@
 SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, COUNT(comments.article_id):: INT AS comment_count 
 FROM articles
 LEFT JOIN comments ON articles.article_id = comments.article_id
-GROUP BY articles.article_id
-ORDER BY articles.created_at DESC;
+AND articles.article_id = 2
+GROUP BY articles.article_id;
 
+SELECT articles.*, COUNT(comments.article_id)
+      AS comment_count
+      FROM comments
+      RIGHT JOIN articles
+      ON articles.article_id = comments.article_id
+      AND comments.article_id=2
+      GROUP BY articles.article_id;
