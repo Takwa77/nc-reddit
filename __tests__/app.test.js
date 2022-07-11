960be3ea48c7e28014c481e5f498485b6894a778
Takwa77 <takwa-t@hotmail.com>
2022-07-11 21:07:24 +0100

    remove unnecessary require declaration

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,6 @@ const testData = require("../db/data/test-data");
 const db = require("../db/connection");
 const request = require("supertest");
 const app = require("../app");
-const { get } = require("express/lib/response");
 
 beforeEach(() => {
   return seed(testData);
